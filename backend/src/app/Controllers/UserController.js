3aec44b6169a9f2f9d5351c8ea145d4de9b2403d
Junior Alves <jjalves96@gmail.com>
2020-04-01 11:03:35 -0300

    chore: UserController update, incomplete

--- backend/src/app/Controllers/UserController.js
+++ backend/src/app/Controllers/UserController.js
@@ -35,6 +35,26 @@ class UserController {
       email,
     });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required().oneOf([Yup.ref('password')]) : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+  }
 }
 
 export default new UserController();
