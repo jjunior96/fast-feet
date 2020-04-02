a43b4c3ff00e1233b9cc1d71fbd4d2bd01894725
Junior Alves <jjalves96@gmail.com>
2020-04-02 20:04:50 -0300

    feat: admin route recipients create

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,8 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
@@ -11,6 +13,9 @@ routes.post('/sessions', SessionController.store);
 
 routes.get('/recipients/:id', RecipientController.show);
 routes.get('/recipients', RecipientController.index);
+
+// Rotas privadas
+routes.use(authMiddleware);
 
 routes.post('/recipients', RecipientController.store);
 
