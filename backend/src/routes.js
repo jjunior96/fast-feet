a5a10682e27c199a24d38b740c1be053157e313f
Junior Alves <jjalves96@gmail.com>
2020-04-10 16:14:17 -0300

    add multerConfig

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
-// import FileController from './app/controllers/FileController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -11,6 +11,8 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.post('/files', upload.single('file', FileController.store));
 
 routes.get('/recipients/:id', RecipientController.show);
 routes.get('/recipients', RecipientController.index);
